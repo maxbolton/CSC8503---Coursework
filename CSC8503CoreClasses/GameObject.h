e116d170dcf527bfc448d9baf92975e092199f57
maxbolton <maxwell.bolton@mail.com>
2024-11-25 11:29:16 +0000

    Add project files.

---
+++ CSC8503CoreClasses/GameObject.h
@@ -0,0 +1,113 @@
+#pragma once
+#include "Transform.h"
+#include "CollisionVolume.h"
+
+using std::vector;
+
+namespace NCL::CSC8503 {
+	class NetworkObject;
+	class RenderObject;
+	class PhysicsObject;
+
+	enum class CollisionLayer {
+		Skybox = 0,
+		Terrain = 1,
+		Water = 2,
+		Buildings = 3,
+		AI = 4,
+		Objects = 5,
+		Player = 6
+	};
+
+
+	class GameObject	{
+	public:
+		GameObject(const std::string& name = "");
+		~GameObject();
+
+		void SetBoundingVolume(CollisionVolume* vol) {
+			boundingVolume = vol;
+		}
+
+		const CollisionVolume* GetBoundingVolume() const {
+			return boundingVolume;
+		}
+
+		bool IsActive() const {
+			return isActive;
+		}
+
+		CollisionLayer getCollisionLayer() const {
+			return colLayer;
+		}
+
+		void SetCollisionLayer(CollisionLayer layer) {
+			colLayer = layer;
+		}
+
+		Transform& GetTransform() {
+			return transform;
+		}
+
+		RenderObject* GetRenderObject() const {
+			return renderObject;
+		}
+
+		PhysicsObject* GetPhysicsObject() const {
+			return physicsObject;
+		}
+
+		NetworkObject* GetNetworkObject() const {
+			return networkObject;
+		}
+
+		void SetRenderObject(RenderObject* newObject) {
+			renderObject = newObject;
+		}
+
+		void SetPhysicsObject(PhysicsObject* newObject) {
+			physicsObject = newObject;
+		}
+
+		const std::string& GetName() const {
+			return name;
+		}
+
+		virtual void OnCollisionBegin(GameObject* otherObject) {
+			//std::cout << "OnCollisionBegin event occured!\n";
+		}
+
+		virtual void OnCollisionEnd(GameObject* otherObject) {
+			//std::cout << "OnCollisionEnd event occured!\n";
+		}
+
+		bool GetBroadphaseAABB(Vector3&outsize) const;
+
+		void UpdateBroadphaseAABB();
+
+		void SetWorldID(int newID) {
+			worldID = newID;
+		}
+
+		int		GetWorldID() const {
+			return worldID;
+		}
+
+	protected:
+		CollisionLayer		colLayer;
+
+		Transform			transform;
+
+		CollisionVolume*	boundingVolume;
+		PhysicsObject*		physicsObject;
+		RenderObject*		renderObject;
+		NetworkObject*		networkObject;
+
+		bool		isActive;
+		int			worldID;
+		std::string	name;
+
+		Vector3 broadphaseAABB;
+	};
+}
+
